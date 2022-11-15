5ecff8047684e9733b5c05e6656245b0983c4f75
Tyler Inari <github@al.tyler.jp>
2022-11-15 11:37:44 -0800

    maybe done cw3

---
+++ Tyler_CW3/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import CalBody from './components/CalBody.vue';
+</script>
+
+<template>
+  <main>
+    <CalBody />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    CalBody
+  }
+}
+</script>
+
+<!-- <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style> -->
---
+++ Tyler_CW3/src/components/CalBody.vue
@@ -0,0 +1,67 @@
+<script setup>
+</script>
+
+<template>
+    <div class="display">
+        <div class="from unit-set">
+            <input type="number" v-model="from_num">
+            <select v-model="from_unit">
+                <option v-for="value, unit in units" :key="unit" :value="unit">{{ unit }}</option>
+            </select>
+        </div>
+        <div class="to unit-set">
+            <!-- <input type="number" v-model="to_num"> -->
+            <p>{{ to_num }}</p>
+            <select v-model="to_unit">
+                <option v-for="value, unit in units" :key="unit" :value="unit">{{ unit }}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CalBody',
+    data() {
+        return {
+            units: {
+                'Nanometers': 1,
+                'Microns': 1000,
+                'Millimeters': 1000000,
+                'Centimeters': 10000000,
+                'Meters': 1000000000,
+                'Kilometers': 1000000000000,
+                'Inches': 25400000,
+                'Feet': 304800000,
+                'Yards': 914400000,
+                'Miles': 1609344000000,
+                'Nautical Miles': 1852000000000
+            },
+            from_num: 0,
+            from_unit: 'Nanometers',
+            to_num: 0,
+            to_unit: 'Nanometers'
+        }
+    },
+    computed: {
+        to_num: function () {
+            return this.from_num * this.units[this.from_unit] / this.units[this.to_unit];
+        }
+    }
+}
+</script>
+
+<style scoped>
+.unit-set {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: space-between;
+}
+
+input,
+select {
+    border: 0px;
+    border-radius: 3px;
+}
+</style>
